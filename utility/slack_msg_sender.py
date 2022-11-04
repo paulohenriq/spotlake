fa69bf5ab6db5dfae58a388e326b332ad7282d97
kh3654po <kh3654po@gmail.com>
2022-11-04 20:56:04 +0900

    send_slack_message()에메시지만 넘겨주면 자동으로 호출한 모듈, 함수, 라인을 같이 출력하도록 코드수정

--- utility/slack_msg_sender.py
+++ utility/slack_msg_sender.py
@@ -1,11 +1,17 @@
 import requests
+import inspect
 
 url = "https://hooks.slack.com/services/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"
 
 
 def send_slack_message(msg):
+    module_name = inspect.stack()[1][1]
+    line_no = inspect.stack()[1][2]
+    function_name = inspect.stack()[1][3]
+
+    message = f"File \"{module_name}\", line {line_no}, in {function_name} :\n{msg}"
 
     slack_data = {
-        "text": msg
+        "text": message
     }
     requests.post(url, json=slack_data)
