d806acabe84271c819e5618021d685480f6d82bb
kh3654po <kh3654po@gmail.com>
2022-11-21 21:11:20 +0900

    람다 환경변수 및 파라미터 스토어 수정

--- utility/slack_msg_sender.py
+++ utility/slack_msg_sender.py
@@ -24,9 +24,12 @@ def send_slack_message(msg):
 def get_webhook_url():
     try:
         ssm = boto3.client('ssm')
-        parameter = ssm.get_parameter(Name="/url/slack/system-monitoring", WithDecryption=False)
+        parameter = ssm.get_parameter(Name="error-notification-slack-webhook-url", WithDecryption=False)
         url = parameter['Parameter']['Value']
     except:
-        url = os.environ.get('webhookURL')
+        url = os.environ.get('error-notification-slack-webhook-url')
 
     return url
+
+
+send_slack_message("testtest")
